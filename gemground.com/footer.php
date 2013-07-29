f51e030b3a6bdb3097e1d414ee397eb56001355b
ziv yuan <ziv.yuan@gmail.com>
2013-07-29 20:35:52 +0800

    base version

---
+++ gemground.com/footer.php
@@ -0,0 +1,19 @@
+	<footer><div class="cwrap">
+		<ul id="footernav" class="list-hor">
+			<li><a href="javascript:void(0);"><img src="images/gem01_26-92.jpg" /></a></li>
+			<li><img src="images/gem01_107.jpg" /></li>
+			<li><a href="javascript:void(0);"><img src="images/gem01_28-93.jpg" /></a></li>
+			<li><img src="images/gem01_107.jpg" /></li>
+			<li><a href="http://e.weibo.com/gemground/"><img src="images/gem01_32-95.jpg" /></a></li>
+			<li><img src="images/gem01_107.jpg" /></li>
+			<li><a href="javascript:void(0);"><img src="images/gem01_34-96.jpg" /></a></li>
+		</ul>
+		<div id="contactinfo" class="">
+			<img src="images/gem01_37-97.jpg" />
+		</div>
+	</div></footer>
+	<!-- scripts -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ gemground.com/index.php
@@ -0,0 +1,117 @@
+<?php
+include_once('header.php');
+
+
+?>	<section id="philosophy" class="thefirstsec">
+		<a name="philosophy"></a>
+		<div class="cwrap">
+			<img src="images/gem01_16.jpg" />
+		</div>
+	</section>
+	<section id="pointofview">
+		<a name="pointofview"></a>
+		<div class="sectitle"><img src="images/gem01_20.jpg" /></div>
+		<div class="cwrap">
+			<img src="images/gem01_20-17.jpg" style="padding-top:50px;"/>
+		</div>
+	</section>
+	<section id="service">
+		<a name="service"></a>
+		<div class="sectitle"><img src="images/gem01_29.jpg" /></div>
+		<div class="cwrap">
+			<img src="images/gem01_38.jpg" style="margin-top:47px;"/>
+			<img src="images/gem01_36.jpg" style="margin-top:30px;" />
+			<img src="images/gem01_32.jpg"" />
+		</div>
+	</section>
+	<section id="clients">
+		<a name="clients"></a>
+		<div class="sectitle"><img src="images/gem01_45.jpg" /></div>
+		<div class="cwrap">
+			<ul id="clientslist">
+				<li><img src="images/logo.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-43.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-44.jpg" width="151" height="60" alt=""></li>
+				<li><img src="images/logo-45.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-46.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-47.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-49.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-50.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-51.jpg" width="151" height="60" alt=""></li>
+				<li><img src="images/logo-52.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-53.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-54.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-55.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-56.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-57.jpg" width="151" height="60" alt=""></li>
+				<li><img src="images/logo-58.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-59.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-60.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-62.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-63.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-64.jpg" width="151" height="60" alt=""></li>
+				<li><img src="images/logo-65.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-66.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-67.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-69.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-70.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-71.jpg" width="151" height="60" alt=""></li>
+				<li><img src="images/logo-72.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-73.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-74.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-76.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-77.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-78.jpg" width="151" height="60" alt=""></li>
+				<li><img src="images/logo-79.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-80.jpg" width="150" height="60" alt=""></li>
+				<li><img src="images/logo-81.jpg" width="150" height="60" alt=""></li>
+			</ul>
+		</div>
+	</section>
+	<section id="works">
+		<a name="works"></a>
+		<div class="sectitle"><img src="images/gem01_83.jpg" /></div>
+		<div class="cwrap">
+			<ul id="worklist" class="list-hor">
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<!-- row -->
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<!-- row -->
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+				<li><div class="workitem">work</div></li>
+			</ul>
+		</div>
+	</section>
+	<section id="about">
+		<a name="about"></a>
+		<div class="sectitle"><img src="images/gem01_83.jpg" /></div>
+		<div class="cwrap">
+			<img src="images/gem01_83-89.jpg" />
+			<ul id="memlist" class="list-hor">
+				<li><a class="teamicon team-sol" target="_blank" href="http://weibo.com/soooool"><span class="mname">刘洋</span></a></li>
+				<li><a class="teamicon team-11" target="_blank" href="http://weibo.com/lemo11"><span class="mname">姚珧</span></a></li>
+				<li><a class="teamicon team-gozi" target="_blank" href="http://weibo.com/ziv84"><span class="mname">果子</span></a></li>
+				<li><a class="teamicon team-dou" target="_blank" href="http://weibo.com/xiaodoudeal"><span class="mname">小豆</span></a></li>
+				<li><a class="teamicon team-jie" target="_blank" href="http://weibo.com/u/1853933265"><span class="mname">小洁</span></a></li>
+			</ul>
+		</div>
+	</section>
+<?php
+include_once('footer.php');
+
+
+?>
