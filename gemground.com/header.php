9c2220a43bbf68a0bc8ab07ca3e75157aec43dea
ziv yuan <ziv.yuan@gmail.com>
2013-08-01 00:29:35 +0800

    更新导航效果 [服务流程],[客户列表],[成员表]居中对齐

--- gemground.com/header.php
+++ gemground.com/header.php
@@ -21,22 +21,26 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-     
-          <!-- Be sure to leave the brand out there if you want it shown -->
-          <span class="brand span2">
-            <img class="cn" src="images/gem-logo.png" />
-          </span>
-     
-          <!-- Everything you want hidden at 940px or less, place within here -->
-          <div class="nav-collapse collapse">
-            <!-- .nav, .navbar-search, .navbar-form, etc -->
-              <ul class="nav">
-                <li><a id="navhome" href="#banner"><span class="aname">首页</span></a></li>
-                <li><a id="navservice" href="#service"><span class="aname">服务与方法</span></a></li>
-                <li><a id="navworks" href="#clients"><span class="aname">案例经验</span></a></li>
-                <li><a id="navclients" href="#about"><span class="aname">关于我们</span></a></li>
-           <!-- <li><p class="aname">・品牌咨询・标志设计・VI 设计・网站设计・APP设计・微信营销・400 1234 567・</p></li>-->
-              </ul>
+          <div class="row">
+            <!-- Be sure to leave the brand out there if you want it shown -->
+            <div class="span3 gemlogowrap">
+              <div id="gemlogo" class="biglogo">
+                <img class="biglogo" src="images/gem-logo.png" />
+                <img class="smalllogo" src="images/gem-logo-small.png" />
+              </div>
+            </div>
+
+            <!-- Everything you want hidden at 940px or less, place within here -->
+            <div class="nav-collapse collapse span9">
+              <!-- .nav, .navbar-search, .navbar-form, etc -->
+                <ul class="nav">
+                  <li><a id="navhome" href="#banner"><span class="aname">首页</span></a></li>
+                  <li><a id="navservice" href="#service"><span class="aname">服务与方法</span></a></li>
+                  <li><a id="navworks" href="#clients"><span class="aname">案例经验</span></a></li>
+                  <li><a id="navclients" href="#about"><span class="aname">关于我们</span></a></li>
+             <!-- <li><p class="aname">・品牌咨询・标志设计・VI 设计・网站设计・APP设计・微信营销・400 1234 567・</p></li>-->
+                </ul>
+            </div>
           </div>
     </div>
   </div>
--- gemground.com/js/gemglobal.js
+++ gemground.com/js/gemglobal.js
@@ -8,43 +8,47 @@ $(function (){
 		var top = $(window).scrollTop(),
 		wh = $(window).height() * 0.6;
 		if(top < wh && nav_status == 'min'){
-			$('header .navbar').css('overflow','hidden').animate({
+			// set nav to max view
+			$('header .navbar').animate({
 				top:0,
 				height:60
 			});
-			$('header .brand').animate({top:0});
+			$('#gemlogo').removeClass('smalllogo').addClass('biglogo');
 			nav_status = 'max';
 		}else if(top > wh && nav_status == 'max'){
-			$('header .navbar').css('overflow','hidden').animate({
+			$('header .navbar').animate({
 				top:-10,
 				height:50
 			});
-			$('header .brand').animate({top:-47});
+			var logo = $('header .brand');
+			$('#gemlogo').removeClass('biglogo').addClass('smalllogo');
 			nav_status = 'min';
 		}
-	},
+	}/*,
+		
 	_hdl_resize = function (){
 		var win = $(window),
 		// Update section min height
 		sech = win.height() * 0.8 - 60,
 		ww = win.width();
 
-		if (sech < 520) {
-			sech = 520;
+
+
+		if (ww < 980) {
+			$('#banner,#service,#pointofview,#clients,#about').height('auto');
+		}else {
+			if (sech < 500) {
+				sech = 500;
+			}else if (sech > 720) {
+				sech = 720;
+			}
+			$('#banner,#service,#pointofview,#clients,#about').height(sech);
 		}
-		$('#banner,#service,#pointofview,#clients,#about').height(sech);
-
-		/*if (ww < 785) {
-			$('#flow').animate({right:-200});	
-		}else{
-			$('#flow').animate({right:30});	
-		}*/
-		$('#flow').css('display', ww < 790 ? 'none' : 'block');
-	};
+		
+	}*/;
 	
-	$(window).resize(_hdl_resize);
+	// $(window).resize(_hdl_resize);
 	$(window).scroll(_hdl_scroll);
-	$('header .brand').css('position', 'relative');
 
-	_hdl_resize();
+	// _hdl_resize();
 });
