70862c2296dedec61f042e6407e5b650a885c300
Sol.liu <Sol.liu@qq.com>
2013-07-31 11:34:05 +0800

    0730

--- gemground.com/header.php
+++ gemground.com/header.php
@@ -7,70 +7,14 @@
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
     <link href="css/global.css" rel="stylesheet" media="screen">
-    <!-- SlidesJS Required: These styles are required if you'd like a responsive slideshow -->
-    <style type="text/css">
-    #slides {
-      display: none
-    }
-
-    .slide-container {
-      margin: 0 auto
-    }
-
-    /* For tablets & smart phones */
-    @media (max-width: 767px) {
-      .slide-container {
-        width: auto
-      }
-    }
-
-    /* For smartphones */
-    @media (max-width: 480px) {
-      .slide-container {
-        width: auto
-      }
-    }
-
-    /* For smaller displays like laptops */
-    @media (min-width: 768px) and (max-width: 979px) {
-      .slide-container {
-        width: 724px
-      }
-    }
-
-    /* For larger displays */
-    @media (min-width: 1200px) {
-      .slide-container {
-        width: 1170px
-      }
-    }
-    </style>
-<!-- SlidesJS Required: -->
-
-	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
 	<script src="js/jquery.1.10.2.min.js"></script>
 	<script src="js/jquery.slides.min.js"></script>
   </head>
   <body>
 	<header>
-  <!--<div class="container">
-		<nav id="mainnav">
-			<ul class="row">
-        <li class="span2"><img src="images/gem-logo.png" /></li>
-        <ul class="span8 offset2">
-  				<li ><a id="navhome" href="#banner"><span class="aname">首页</span></a></li>
-  				<li ><a id="navpointofview" href="#pointofview"><span class="aname">观点</span></a></li>
-  				<li><a id="navservice" href="#service"><span class="aname">服务</span></a></li>
-  				<li><a id="navworks" href="#clients"><span class="aname">客户</span></a></li>
-  				<li><a id="navclients" href="#about"><span class="aname">关于聚广</span></a></li>
-  				<li><i>400 1234 567</i></li>
-        </ul>
-			</ul>
-		</nav>
-	</div>-->
   <div class="navbar navbar-fixed-top">
     <div class="navbar-inner" id="mainnav">
-      <div class="container">
+      <div class="container"id="gemnav">
           <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
@@ -86,11 +30,10 @@
             <!-- .nav, .navbar-search, .navbar-form, etc -->
               <ul class="nav">
                 <li ><a id="navhome" href="#banner"><span class="aname">首页</span></a></li>
-                <li ><a id="navpointofview" href="#pointofview"><span class="aname">观点</span></a></li>
-                <li><a id="navservice" href="#service"><span class="aname">服务</span></a></li>
-                <li><a id="navworks" href="#clients"><span class="aname">客户</span></a></li>
-                <li><a id="navclients" href="#about"><span class="aname">关于聚广</span></a></li>
-                <li><p class="aname">400 1234 567</p></li>
+                <li><a id="navservice" href="#service"><span class="aname">服务与方法</span></a></li>
+                <li><a id="navworks" href="#clients"><span class="aname">案例经验</span></a></li>
+                <li><a id="navclients" href="#about"><span class="aname">关于我们</span></a></li>
+                <li><p class="aname">立即咨询：400 1234 567</p></li>
               </ul>
           </div>
     </div>
--- gemground.com/index.php
+++ gemground.com/index.php
@@ -2,10 +2,11 @@
 require_once('header.php');
 
 ?>	
+<div class="container" id="cwrap">
 	<div id="banner" class="carousel slide container">
 	  <ol class="carousel-indicators">
-	    <li data-target="#banner" data-slide-to="0" class="active"></li>
-	    <li data-target="#banner" data-slide-to="1"></li>
+	    <li data-target="#banner" data-slide-to="0"></li>
+	    <li data-target="#banner" data-slide-to="1" class="active"></li>
 	    <li data-target="#banner" data-slide-to="2"></li>
 	  </ol>
 	  <!-- Carousel items -->
@@ -20,58 +21,79 @@ require_once('header.php');
 	</div>
 	<section id="pointofview" class="container">
 		<a name="pointofview"></a>
-			<div class="sectitle">聚广的观点</div>
+			<div class="sectitle">观点</div>
 			<div class="row">
-				<img src="images/gem01_20-17.jpg" style="padding-top:50px;"/>
+				
 			</div>
 	</section>
 	<section id="service" class="container">
 		<a name="service"></a>
-			<div class="sectitle">我们的服务</div>
-			<div class="row">
-				<img src="images/gem01_38.jpg" style="margin-top:47px;"/>
-				<img src="images/gem01_36.jpg" style="margin-top:30px;" />
-				<img src="images/gem01_32.jpg" />
+			<div class="sectitle">核心产品</div>
+			<div class="row" id="products">
+				<div class="span3 offset1">
+					<li><h3>「快塑」</h3></li>
+					<li><i>专为新品牌，快速成长，快速占领</i></li>
+					<li>・</li>
+					<li>内部激活建议</li>
+					<li>外部启动计划</li>
+					<li>品牌启动手册</li>
+					<li>传播效果评估</li>
+					<li>・</li>
+					<li><span>快速打造，直击目标客户！</span></li>
+				</div>
+				<div class="span4">
+					<li><h3>「焕新」</h3></li>
+					<li><i>针对老品牌，摆脱困境，拥抱蓝海</i></li>
+					<li>・</li>
+					<li>品牌现状评估</li>
+					<li>品牌焕新计划</li>
+					<li>品牌焕新手册</li>
+					<li>品牌焕新传播效果评估</li>
+					<li>・</li>
+					<li><span>聚广为老品牌倾力打造的服务整合，让品牌重焕活力！</span></li>
+				</div>
+				<div class="span3">
+					<li><h3>「微智」</h3></li>
+					<li><i>智能展会整合解决方案</i></li>
+					<li>・</li>
+					<li>会展前期策划</li>
+					<li>互联网平台部署</li>
+					<li>现场互动活动策划</li>
+					<li>展会数据跟踪及分析</li>
+					<li>・</li>
+					<li><span>精细化、低成本，信息交换更便捷，会展效果更持久</span></li>
+				</div>
 			</div>
-		</div>
 	</section>
-	<div id="clients-bg">
 	<section id="clients" class="container">
 		<a name="clients"></a>
 		<div class="sectitle">聚广成员曾服务过的品牌</div>
 		<div class="row">
-			<li class="span2"><img src="images/clients-01.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-02.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-03.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-04.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-05.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-06.jpg" alt=""></li>
-		</div>
-		<div class="row">
-			<li class="span2"><img src="images/clients-07.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-08.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-09.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-10.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-11.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-12.jpg" alt=""></li>
-		</div>
-		<div class="row">	
-			<li class="span2"><img src="images/clients-13.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-14.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-15.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-16.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-17.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-18.jpg" alt=""></li>
-		</div>
-		<div class="row">	
-			<li class="span2"><img src="images/clients-19.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-20.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-21.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-22.jpg" alt=""></li>
-			<li class="span2"><img src="images/clients-23.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-01.jpg" alt="">
+			<img src="images/clients-02.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-03.jpg" alt="">
+			<img src="images/clients-04.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-05.jpg" alt="">
+			<img src="images/clients-06.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-07.jpg" alt="">
+			<img src="images/clients-08.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-09.jpg" alt="">
+			<img src="images/clients-10.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-11.jpg" alt="">
+			<img src="images/clients-12.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-13.jpg" alt="">
+			<img src="images/clients-14.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-15.jpg" alt="">
+			<img src="images/clients-16.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-17.jpg" alt="">
+			<img src="images/clients-18.jpg" alt=""></li>	
+			<li class="span4"><img src="images/clients-19.jpg" alt="">
+			<img src="images/clients-20.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-21.jpg" alt="">
+			<img src="images/clients-22.jpg" alt=""></li>
+			<li class="span4"><img src="images/clients-23.jpg" alt=""></li>
 		</div>
 	</section>
-	</div>
 	<section id="about" class="container"> 
 		<a name="about"></a>
 			<div class="row">
@@ -80,14 +102,15 @@ require_once('header.php');
 					<p>聚广由一群精通品牌营销、热爱互联网的人组成，我们运用互联网思维为品牌服务。<br/>聚广人在品牌营销、互联网、电子商务领域都有丰富的实战经验，能为客户提供量身定做的完整解决方案。</p>
 					<p>聚广人坚信，互联网思维将彻底改变品牌营销。<br/>受众研究、数据挖掘、数据分析将从每个环节上对品牌策略提供支持， 让营销成本更低廉、更透明， 让效果可预测、更精准、更可控。</p>
 					<h3>聚广，品牌数字整合营销专家。</h3>
+					<p>品牌咨询｜标志设计｜VI 设计｜网站设计｜APP 设计｜微信营销</p>
 				</div>
 			</div>
 
 			<div id="memlist" class="row">
-				<li class="span2"><a class="teamicon team-sol" target="_blank" href="http://weibo.com/soooool"></a></li>
-				<li class="span2"><a class="teamicon team-11" target="_blank" href="http://weibo.com/lemo11"></a></li>
+				<li class="span2 offset1"><a class="teamicon team-11" target="_blank" href="http://weibo.com/lemo11"></a></li>
 				<li class="span2"><a class="teamicon team-gozi" target="_blank" href="http://weibo.com/ziv84"></a></li>
 				<li class="span2"><a class="teamicon team-dou" target="_blank" href="http://weibo.com/xiaodoudeal"></a></li>
+				<li class="span2"><a class="teamicon team-sol" target="_blank" href="http://weibo.com/soooool"></a></li>
 				<li class="span2"><a class="teamicon team-jie" target="_blank" href="http://weibo.com/u/1853933265"></a></li>
 			</div>
 	</section>
