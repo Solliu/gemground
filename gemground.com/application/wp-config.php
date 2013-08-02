ba1bd505b8b44478796a23a52f6295836c33dc43
ziv yuan <ziv.yuan@gmail.com>
2013-08-02 19:05:28 +0800

    remove config-sample.php file update database user name backup base database

--- gemground.com/application/wp-config.php
+++ gemground.com/application/wp-config.php
@@ -15,22 +15,45 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress 数据库的名称 */
-define('DB_NAME', 'gemground');
+if ($_SERVER['HTTP_REQUESR'] == 'localhost') {
 
-/** MySQL 数据库用户名 */
-define('DB_USER', 'root');
+	define('DB_NAME', 'gemground');
 
-/** MySQL 数据库密码 */
-define('DB_PASSWORD', '');
+	/** MySQL 数据库用户名 */
+	define('DB_USER', 'root');
 
-/** MySQL 主机 */
-define('DB_HOST', 'localhost');
+	/** MySQL 数据库密码 */
+	define('DB_PASSWORD', '');
 
-/** 创建数据表时默认的文字编码 */
-define('DB_CHARSET', 'utf8');
+	/** MySQL 主机 */
+	define('DB_HOST', 'localhost');
 
-/** 数据库整理类型。如不确定请勿更改 */
-define('DB_COLLATE', '');
+	/** 创建数据表时默认的文字编码 */
+	define('DB_CHARSET', 'utf8');
+
+	/** 数据库整理类型。如不确定请勿更改 */
+	define('DB_COLLATE', '');
+
+}else{
+
+	define('DB_NAME', 'gemground');
+
+	/** MySQL 数据库用户名 */
+	define('DB_USER', 'flyingwikime');
+
+	/** MySQL 数据库密码 */
+	define('DB_PASSWORD', '4!PRfW7i');
+
+	/** MySQL 主机 */
+	define('DB_HOST', 'mysql.duyujun.com');
+
+	/** 创建数据表时默认的文字编码 */
+	define('DB_CHARSET', 'utf8');
+
+	/** 数据库整理类型。如不确定请勿更改 */
+	define('DB_COLLATE', '');
+
+}
 
 /**#@+
  * 身份认证密匙设定。
